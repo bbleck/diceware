502d74646e560282dba4132537bd94e929e331cb
brianb <bbleck@unm.edu>
2018-10-04 13:49:36 -0600

    Added filters to the bundle keysets.

--- src/edu/cnm/deepdive/cryptography/BundleSource.java
+++ src/edu/cnm/deepdive/cryptography/BundleSource.java
@@ -11,8 +11,10 @@ public class BundleSource implements WordSource {
   public BundleSource(String baseName) {
     ResourceBundle bundle = ResourceBundle.getBundle(baseName);
     words = new ArrayList<>(
-        bundle.keySet().stream()//create stream of keys(string)
-            .map((k) -> bundle.getString(k))//map lets us transform types, returns a stream of strings
+        bundle.keySet().stream()//create stream of keys(in a Resource Bundle, keys are always Strings, but they may not always be in other kinds of maps)
+            .map((k) -> bundle.getString(k))//map lets us transform types, returns a stream of strings; BEFORE WE MAPPED, ALL WE HAD WERE KEYS
+            .filter((s) -> !s.matches("^.*\\W|\\d.*$"))//filter out words with punctuation or non-word character
+            .filter((s) -> s.length() > 3)
             .collect(Collectors.toList())//collect lets us collect the items into a different kind of structure, returns a list of strings, which we will use as the argument to our ArrayList constructor, which will create an ArrayList that contains all of our items
     );
   }
